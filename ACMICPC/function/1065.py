ecb2398e1a660469492fcc28c80ecc09c81aa8a8
Googoles <googoles1@gmail.com>
2020-04-05 15:25:43 +0900

    I just finished only one problem today... because of assignments.

---
+++ ACMICPC/function/1065.py
@@ -0,0 +1,7 @@
+n = int(input())
+nums = set(range(1,n))
+cnt = 0
+# for i in range(n):
+    #
+
+print(nums)
--- ACMICPC/function/4673.py
+++ ACMICPC/function/4673.py
@@ -1,4 +1,9 @@
-
-
-# 10을 기준으로 5개 출력
-# if를 써서
+num = set(range(1,10001)) # 배열생성
+gen_num = set() #생성하고싶은 배열
+for i in range(1,10001): # 10000까지 반복
+    for j in str(i):
+        i += int(j)
+    gen_num.add(i)
+self_num = num - gen_num
+for k in sorted(self_num):
+    print(k)
