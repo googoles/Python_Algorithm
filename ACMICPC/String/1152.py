06f8a2c37e337ff79bcf1d93e6b2068b526d6ca8
Googoles <googoles1@gmail.com>
2020-04-07 23:02:49 +0900

    I just finished only one problem today... because of assignments.

--- ACMICPC/String/1152.py
+++ ACMICPC/String/1152.py
@@ -5,22 +5,18 @@ start = time.time()
 import sys
 st = list(map(str, sys.stdin.readline().split()))
 print(len(st))
-# cnt = 0
-# # save = []
-# # st.sort()
-# print(st)
+cnt = 0
+# save = []
+# st.sort()
+print(st)
 # for i in range(len(st)):
-#     if st[i] == st[i-1]:
-#         continue
-#     else:
-#         cnt += 1
-# print(cnt)
-# for i in range(len(st)):
-#     print(st)
-    # if st[i] in st[:i]:
-    #     continue
-    # else:
-    #     cnt += 1
+
+for i in range(len(st)):
+    print(st)
+    if st[i] in st[:i]:
+        continue
+    else:
+        cnt += 1
 # print(cnt)
 #         # save.append(st[i])
 # # print(len(save))
--- ACMICPC/function/1065.py
+++ ACMICPC/function/1065.py
@@ -1,7 +1,11 @@
-n = int(input())
-nums = set(range(1,n))
-cnt = 0
-# for i in range(n):
-    #
+# n = int(input())
+# nums = set(range(1,n))
+# cnt = 0
+# # for i in range(n):
+#     #
+#
+# print(nums)
 
-print(nums)
+sentence = 'aasdf famsdklf amfskdlf asdf asdmkfl'
+
+print(sentence.split(' '))
