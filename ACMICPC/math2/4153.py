f58e4352a2b2b812f1cb5550eb4d21d0be276795
Googoles <googoles1@gmail.com>
2020-05-26 20:40:05 +0900

    Prepare Coding test

--- ACMICPC/math2/4153.py
+++ ACMICPC/math2/4153.py
@@ -1,7 +1,8 @@
+while 1:
 
-a = list(map(int,input().split(' ')))
+    a = list(map(int,input().split(' ')))
 
-if (a[0] ** 2 + a[1] ** 2) == a[2] ** 2:
-    print('right')
-else:
-    print('wrong')
+    if (a[0] ** 2 + a[1] ** 2) == a[2] ** 2:
+        print('right')
+    else:
+        print('wrong')
